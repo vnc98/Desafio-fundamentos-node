80c15600c2d62d43873f8b444e7662bd664b2b97
Vinicius Catarino <vinicatarino@gmail.com>
2020-11-10 22:17:02 -0300

    Finish challenge 05

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,34 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+    title: string;
+    type: 'income' | 'outcome';
+    value: number;
+}
+
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+    private transactionsRepository: TransactionsRepository;
 
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
+    constructor(transactionsRepository: TransactionsRepository) {
+        this.transactionsRepository = transactionsRepository;
+    }
 
-  public execute(): Transaction {
-    // TODO
-  }
+    public execute({ title, type, value }: TransactionDTO): Transaction {
+        const { total } = this.transactionsRepository.getBalance();
+
+        if (type === 'outcome' && total < value) {
+            throw new Error('You do not have enought balance');
+        }
+
+        const transaction = this.transactionsRepository.create({
+            title,
+            type,
+            value,
+        });
+
+        return transaction;
+    }
 }
 
 export default CreateTransactionService;
